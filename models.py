9d08e7a3f02bdd60111b70d2f90817ad53ceb2a8
KrzMa <marut.krzysztof6@gmail.com>
2023-05-21 14:25:11 +0200

    initial commit

---
+++ models.py
@@ -0,0 +1,26 @@
+import datetime
+
+from sqlalchemy import Column, Integer, create_engine, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine(
+    'mysql+pymysql://root:Luffy123@localhost:3306/blog'
+)
+Base = declarative_base(bind=engine)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    user_name = Column(String(50), unique=True, nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    registration_date = Column(DateTime, default=datetime.datetime.now)
+
+    def __repr__(self):
+        return f'User({self.user_name})'
+
+
+Base.metadata.create_all()
